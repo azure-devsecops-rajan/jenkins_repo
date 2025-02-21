082eef563f3aa67ac5ab7559b707602e546be20d
Name <you@example.com>
2025-02-22 02:46:47 +0530

    rg code added12

--- main.tf
+++ main.tf
@@ -11,16 +11,16 @@ provider "azurerm" {
     subscription_id = "3ba70bac-d8e7-4f2b-8e18-7fa19d2fb0f1"
   
 }
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name   = "mat-delete-krna"
-#     storage_account_name  = "kadirst25"
-#     container_name        = "jenkins"
-#     key                   = "terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "mat-delete-krna"
+    storage_account_name  = "kadirst25"
+    container_name        = "jenkins"
+    key                   = "terraform.tfstate"
+  }
+}
 resource "azurerm_resource_group" "rg" {
-    name     = "myTFResourceGroup"
+    name     = "myTFResourceGroup1"
     location = "East US"
   
 }
