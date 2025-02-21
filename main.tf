d8136beab44ce058de976adb6402b310acf98940
Name <you@example.com>
2025-02-22 03:15:30 +0530

    vnet

--- main.tf
+++ main.tf
@@ -24,3 +24,9 @@ resource "azurerm_resource_group" "rg" {
     location = "East US"
   
 }
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVNet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.rg.location  
+    resource_group_name = azurerm_resource_group.rg.name
+}
